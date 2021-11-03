77c8225bd9a7cd58f71c5f066c91dbfe40465bf5
bourne.yang <bourne.yang@meetsocial.cn>
2021-11-03 17:57:02 +0800

    初步完成单一下载vue-web项目模板

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # vue-web-ready-cli
 基于vue的web项目通用脚手架
+
+npm install -g @vue-web-ready-cli
+ready init my-project
--- bin/ready.js
+++ bin/ready.js
@@ -1,8 +1,6 @@
 #!/usr/bin/env node
+
 const program = require('commander')
-// const download = require('download-git-repo')
-// const chalk = require('chalk')
-// const ora = require('ora')
 
 // 定义当前版本
 // 定义使用方法
@@ -10,11 +8,9 @@ const program = require('commander')
 program
  .version(require('../package').version)
  .usage('<command> [options]')
- .option('-i, init [name]', 'generate a new project from a template')
  .command('init', 'generate a new project from a template')
- //  .command('add', 'add a new template')
-  //  .command('delete', 'delete a template')
-  //  .command('list', 'list all the templates')
+  // 扩展
+  // .command('add', 'add a new template')
+  // .command('delete', 'delete a template')
+  // .command('list', 'list all the templates')
  .parse(process.argv)
-// 解析命令行参数
-// program.parse(process.argv)
